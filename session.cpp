5808a89d5cca35e6ec7d6d5899586857489fe6d7
Rookwood Chau <rchau@un-r2.kzn.21-school.ru>
2022-07-19 16:07:59 +0300

    111

--- session.cpp
+++ session.cpp
@@ -33,6 +33,8 @@ void Session::Receive() {
 
 void Session::Parse() {
 	size_t pos = req.find("\r\n\r\n");
+	if (pos == std::string::npos)
+		return;
 	request.setBody(req.substr(pos + 4));
 	req = req.substr(0, pos+2);
 	std::vector<std::string> h = ft_split(req, "\r\n");
@@ -69,16 +71,6 @@ void Session::SetResponse(){
 	response.setProtocol(request.getProtocol());
 	response.setStatus(response.getStatus());
 	response.setContentType(request);
-	// try
-	// {
-	// 	std::string file_name = this->GetConfigServer().getRoot() + request.getUri();
-	// 	response.setBody(read_file(file_name));
-	// }
-	// catch(const std::exception& e)
-	// {
-	// 	std::cerr << e.what() << std::endl;
-	// 	response.setBody("NOT FOUND\n");
-	// }
 	response.setHeader(response.getProtocol() + response.getStatus() + response.getContentType());
 	response.setHeader("Content-Length: " + std::to_string(response.getBody().size()) + "\n\n");
 	response.setBuffer(response.getHeaders() + response.getBody());
@@ -89,24 +81,12 @@ void Session::Handle(bool r, bool w) {
 	if (!r)
 		return;
 	Receive();
-	try {
-		Parse();
-	}
-	catch(const std::exception& e) {
-		std::cerr << e.what() << std::endl;
-		std::cout << "error in parse" << std::endl;
-	}
+	Parse();
 	std::cout << req << std::endl;
 	RequestHandler rh(this->GetConfigServer(), request, response);
 	rh.Handle();
 	SetResponse();
-	try {
-		Send();
-	}   
-	catch(const std::exception& e) {
-		std::cerr << e.what() << std::endl;
-		std::cout << "error in send" << std::endl;
-	}
+	Send();
 	master->RemoveSession(this);
 
 }
